efa79f08f92ea6ae2bf731df2818c1a1bd7cb021
foyou <35125624+lemisky@users.noreply.github.com>
2026-04-04 20:25:06 +0800

    -

--- .github/scripts/idle-watcher.sh
+++ .github/scripts/idle-watcher.sh
@@ -4,6 +4,8 @@ TIMEOUT=${IDLE_TIMEOUT:-120}
 CHECK_INTERVAL=30
 IDLE_TIME=0
 
+sleep $CHECK_INTERVAL
+
 while sleep $CHECK_INTERVAL; do
     if pgrep -x login > /dev/null \
     || pgrep -x mosh-server > /dev/null; then
